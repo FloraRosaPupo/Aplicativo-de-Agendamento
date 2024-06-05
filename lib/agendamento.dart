6c56c057f1b2dcbcac09130e90525dc6a05b73ce
FloraRosaPupo <florapupo@gmail.com>
2024-06-05 09:45:33 -0300

    estilizando os cards

--- lib/agendamento.dart
+++ lib/agendamento.dart
@@ -102,16 +102,58 @@ class AgendamentoPage extends GetView<CalendarController> {
                             10), // Adiciona um espaço entre o CircleAvatar e o ListView
                     // Envolva o ListView em um Expanded ou forneça uma altura fixa
                     Container(
-                      height: 200, // Define uma altura fixa para o ListView
+                      height: 500, //mudar tamanho
+                      // Define uma altura fixa para o ListView
                       child: ListView.builder(
                         itemCount: controller.agenda.length,
                         itemBuilder: (BuildContext context, int index) {
+                          final event = controller.agenda[index];
+
                           return Card(
-                            child: ListTile(
-                              title: Text(
-                                controller.agenda[index],
-                              ),
-                            ),
+                            color: Colors.white,
+                            elevation: 5,
+                            child: Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '${event.horario.horaInicial} - ${event.horario.horaFinal}',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w300),
+                                        ),
+                                        Text(
+                                          event.nome,
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        )
+                                      ],
+                                    ),
+                                    Row(
+                                      children: [
+                                        IconButton(
+                                            onPressed: () {},
+                                            icon: Icon(Icons.edit_calendar)),
+                                        IconButton(
+                                            onPressed: () {},
+                                            icon: Icon(
+                                              Icons.delete,
+                                              color: Colors.red,
+                                            )),
+                                      ],
+                                    )
+                                  ],
+                                )),
                           );
                         },
                       ),
@@ -134,47 +176,52 @@ class AgendamentoPage extends GetView<CalendarController> {
               textAlign: TextAlign.center,
             ),
             content: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome',
-                      hintText: 'Digite o nome do agendamento...',
-                      focusedBorder: FocusedBorder(),
-                      enabledBorder: EnableBorder(),
+              child: Container(
+                padding: EdgeInsets.only(top: 10),
+                // height: 500,
+                width: 400,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Nome',
+                        hintText: 'Digite o nome do agendamento...',
+                        focusedBorder: FocusedBorder(),
+                        enabledBorder: EnableBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Horário',
-                      hintText: 'ex: 08:00',
-                      focusedBorder: FocusedBorder(),
-                      enabledBorder: EnableBorder(),
+                    SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Horário',
+                        hintText: 'ex: 08:00',
+                        focusedBorder: FocusedBorder(),
+                        enabledBorder: EnableBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Data',
-                      hintText: 'ex: 20/03/2024',
-                      focusedBorder: FocusedBorder(),
-                      enabledBorder: EnableBorder(),
+                    SizedBox(height: 10),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Data',
+                        hintText: 'ex: 20/03/2024',
+                        focusedBorder: FocusedBorder(),
+                        enabledBorder: EnableBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  TextField(
-                    maxLines: 3,
-                    decoration: InputDecoration(
-                      labelText: 'Descrição',
-                      hintText: 'Digite a descrição...',
-                      focusedBorder: FocusedBorder(),
-                      enabledBorder: EnableBorder(),
+                    SizedBox(height: 10),
+                    TextField(
+                      maxLines: 3,
+                      decoration: InputDecoration(
+                        labelText: 'Descrição',
+                        hintText: 'Digite a descrição...',
+                        focusedBorder: FocusedBorder(),
+                        enabledBorder: EnableBorder(),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             actions: <Widget>[
@@ -182,14 +229,18 @@ class AgendamentoPage extends GetView<CalendarController> {
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
-                child: Text('Cancelar'),
+                child: Text(
+                  'Cancelar',
+                  style: TextStyle(fontSize: 17),
+                ),
               ),
               ElevatedButton(
                 onPressed: () {
                   // Implementar a lógica para salvar o agendamento
                   Navigator.of(context).pop();
                 },
-                child: Text('Salvar Agendamento'),
+                child:
+                    Text('Salvar Agendamento', style: TextStyle(fontSize: 17)),
               ),
             ],
           );
@@ -201,7 +252,7 @@ OutlineInputBorder FocusedBorder() {
   return OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(20.0)),
     borderSide: BorderSide(
-      color: Colors.deepPurple,
+      color: Colors.grey,
     ),
   );
 }
@@ -210,7 +261,7 @@ OutlineInputBorder EnableBorder() {
   return OutlineInputBorder(
     borderRadius: BorderRadius.all(Radius.circular(20.0)),
     borderSide: BorderSide(
-      color: Colors.deepPurple,
+      color: Colors.grey,
     ),
   );
 }
--- lib/controller/calendario_controller.dart
+++ lib/controller/calendario_controller.dart
@@ -1,57 +1,130 @@
 import 'package:get/get.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+class Horario {
+  final String horaInicial;
+  final String horaFinal;
+
+  Horario({
+    required this.horaInicial,
+    required this.horaFinal,
+  });
+}
+
+class AgendaEvent {
+  final int id;
+  final String nome;
+  final String descricao;
+  final Horario horario;
+
+  AgendaEvent({
+    required this.id,
+    required this.nome,
+    required this.descricao,
+    required this.horario,
+  });
+}
+
 class CalendarController extends GetxController {
   late DateTime primeiroDia;
   late DateTime ultimoDia;
   late DateTime diaAtual;
 
-  List<String> agenda = [];
+  List<AgendaEvent> agenda = [];
 
   CalendarFormat calendarFormat = CalendarFormat.month;
 
   String locale = 'pt-BR';
 
-  void onInit() async {
+  @override
+  void onInit() {
+    super.onInit();
     primeiroDia = DateTime.utc(2023, 1, 1);
     ultimoDia = DateTime.utc(2030, 12, 31);
     diaAtual = DateTime.now();
 
     getProgramacaoDia(diaAtual);
-    super.onInit();
   }
 
-  Future getProgramacaoDia(data) async {
-    //seta o valor de dia
+  Future<void> getProgramacaoDia(DateTime data) async {
+    // Seta o valor de dia
     diaAtual = data;
 
-    //busca os dados na agenda
+    // Busca os dados na agenda
     getAgenda(data);
 
-    //refrash ui
-    update(['calendario', 'agenda']); //seção das paginas q escolher
+    // Refrash UI
+    update(['calendario', 'agenda']); // Seção das páginas que escolher
   }
 
-  //Vai vir de um banco de dados e mostrar na tela
+  // Vai vir de um banco de dados e mostrar na tela
   void getAgenda(DateTime data) {
-    agenda.clear(); //limpo a agenda
+    agenda.clear(); // Limpa a agenda
 
     if (data.day == 27) {
       agenda = [
-        '09:30:Academia',
-        '10:30:Academia',
-        '12:30:Academia',
+        AgendaEvent(
+          id: 1,
+          nome: 'Academia',
+          descricao: 'Treino de musculação',
+          horario: Horario(
+            horaInicial: '09:30',
+            horaFinal: '10:30',
+          ),
+        ),
+        AgendaEvent(
+          id: 2,
+          nome: 'Academia',
+          descricao: 'Treino de cardio',
+          horario: Horario(
+            horaInicial: '10:30',
+            horaFinal: '11:30',
+          ),
+        ),
+        AgendaEvent(
+          id: 3,
+          nome: 'Academia',
+          descricao: 'Aula de spinning',
+          horario: Horario(
+            horaInicial: '12:30',
+            horaFinal: '13:30',
+          ),
+        ),
       ];
     } else if (data.day == 28) {
       agenda = [
-        '09:30:Treino',
-        '10:30:Academia',
-        '12:30:Academia',
+        AgendaEvent(
+          id: 4,
+          nome: 'Treino',
+          descricao: 'Treino funcional',
+          horario: Horario(
+            horaInicial: '09:30',
+            horaFinal: '10:30',
+          ),
+        ),
+        AgendaEvent(
+          id: 5,
+          nome: 'Academia',
+          descricao: 'Treino de musculação',
+          horario: Horario(
+            horaInicial: '10:30',
+            horaFinal: '11:30',
+          ),
+        ),
+        AgendaEvent(
+          id: 6,
+          nome: 'Academia',
+          descricao: 'Aula de yoga',
+          horario: Horario(
+            horaInicial: '12:30',
+            horaFinal: '13:30',
+          ),
+        ),
       ];
     }
   }
 
-  void onFormatChanged(format) {
+  void onFormatChanged(CalendarFormat format) {
     calendarFormat = format;
 
     update(['calendario']);
